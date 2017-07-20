55f522c9c7a2d0fd879ee5723351ae5e43bf19b0
Павел Страх <pfear@localhost.localdomain>
2017-07-20 21:42:54 +0300

    MISTAKES WERE MADE

--- demo.rb
+++ demo.rb
@@ -6,12 +6,9 @@ require './theatre.rb'
 require './movie.rb'
 require './movie_collection.rb'
 require './netflix.rb'
-#binding.pry
-
-netflix = MovieProduction::Netflix.new
-netflix.show { |movie| !movie.title.include?('Terminator') && movie.genre.include?('Action') && movie.year > 2003}
-#netflix.define_filter(:test) { |movie| movie.director != 'David Fincher' && movie.year > 1999 }
-#netflix.define_filter(:test) { |movie| movie.director != 'David Fincher' && movie.year > 1999 }
+binding.pry
+#netflix = Netflix.new
+#netflix.show(genre: 'Drama', period: 'New')
 #puts movies.stats :month
 #puts movies.filter(genre: 'Comedy', year: 2011)
 #puts movies.filter(actors: /Bob/)
